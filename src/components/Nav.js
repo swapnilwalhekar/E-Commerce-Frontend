0a76b13b029bb69b9a3e45175e49a69a810b6db0
swapnilwalhekar <swapnilwalhekar1999@ggmail.com>
2024-04-21 20:24:17 +0530

    Working on update product

---
+++ src/components/Nav.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+    Link, useNavigate
+} from 'react-router-dom';
+const Nav = () => {
+    const auth = localStorage.getItem('user');
+    const navigate = useNavigate();
+    const logout = () => {
+        localStorage.clear();
+        navigate('/signup')
+    }
+    return (
+        <div>
+            <img
+            alt='logo'
+            className='logo'
+            src="https://i.pinimg.com/474x/4b/16/48/4b1648fe8ab9f1f82ff4e16499144b59.jpg" />
+            {
+                auth ?
+                    <ul className="nav-ul">
+                        <li><Link to="/">Products</Link></li>
+                        <li><Link to="/add">Add Products</Link></li>
+                        <li><Link to="/update"> Update Products</Link></li>
+                        <li><Link to="/profile">Profile</Link></li>
+                        <li> <Link onClick={logout} to="/signup">Logout ({ JSON.parse(auth).name})</Link></li>
+                    </ul>
+                    :
+                    <ul className="nav-ul nav-right">
+                        <li> <Link to="/signup">Sign Up</Link></li>
+                        <li><Link to="/login">Login</Link></li>
+                    </ul>
+            }
+        </div>
+    )
+}
+
+export default Nav;
