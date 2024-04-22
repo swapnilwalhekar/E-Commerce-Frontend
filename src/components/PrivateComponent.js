ffb672669da5768d0688eccf7541ad42552f1748
swapnilwalhekar <swapnilwalhekar1999@ggmail.com>
2024-04-22 22:44:07 +0530

    Minimal changes

--- src/components/PrivateComponent.js
+++ src/components/PrivateComponent.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Navigate, Outlet} from 'react-router-dom'
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
 
-const PrivateComponent= ()=>{
-    const auth = localStorage.getItem('user');
-    
-    return auth ?<Outlet />:<Navigate to="signup" />
-}
+const PrivateComponent = () => {
+  const auth = localStorage.getItem("user");
 
-export default PrivateComponent
+  return auth ? <Outlet /> : <Navigate to="signup" />;
+};
+
+export default PrivateComponent;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,78 +1,84 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 const ProductList = () => {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        getProducts();
-    }, []);
+  useEffect(() => {
+    getProducts();
+  }, []);
 
-    const getProducts = async () => {
-        let result = await fetch('http://localhost:9000/products',{
-            headers:{
-                authorization:JSON.parse(localStorage.getItem('token'))
-            }
-        });
-        result = await result.json();
+  const getProducts = async () => {
+    let result = await fetch("http://localhost:9000/products", {
+      headers: {
+        authorization: JSON.parse(localStorage.getItem("token")),
+      },
+    });
+    result = await result.json();
+    setProducts(result);
+  };
+
+  const deleteProduct = async (id) => {
+    let result = await fetch(`http://localhost:9000/product/${id}`, {
+      method: "Delete",
+    });
+    result = await result.json();
+    if (result) {
+      getProducts();
+    }
+  };
+
+  const updateProduct = async (id) => {
+    navigate("/update/" + id);
+  };
+
+  const searchHandle = async (event) => {
+    let key = event.target.value;
+    if (key) {
+      let result = await fetch(`http://localhost:9000/search/${key}`);
+      result = await result.json();
+      if (result) {
         setProducts(result);
+      }
+    } else {
+      getProducts();
     }
+  };
 
-    const deleteProduct = async (id) => {
-        console.warn(id)
-        let result = await fetch(`http://localhost:9000/product/${id}`, {
-            method: "Delete"
-        });
-        result = await result.json();
-        if (result) {
-            getProducts();
-        }
-    }
-
-    const searchHandle = async (event)=>{
-        let key = event.target.value;
-        if(key){
-            let result = await fetch(`http://localhost:9000/search/${key}`);
-            result = await result.json()
-            if(result){
-                setProducts(result)
-            }
-        }else{
-            getProducts();
-        }
-    }
-
-    return (
-        <div className="product-list">
-            <h3>Product List</h3>
-            <input type="" className='search-product-box' placeholder='Search Product'
-            onChange={searchHandle}
-             />
-            <ul>
-                <li>S. No.</li>
-                <li>Name</li>
-                <li>Price</li>
-                <li>Category</li>
-                <li>Operation</li>
-
-            </ul>
-            {
-                products.length>0 ? products.map((item, index) =>
-                    <ul key={item._id}>
-                        <li>{index + 1}</li>
-                        <li>{item.name}</li>
-                        <li>{item.price}</li>
-                        <li>{item.category}</li>
-                        <li>
-                            <button onClick={() => deleteProduct(item._id)}>Delete</button>
-                            <Link to={"/update/"+item._id} >Update </Link>
-                            </li>
-
-                    </ul>
-                )
-                :<h1>No Result Found</h1>
-            }
-        </div>
-    )
-}
+  return (
+    <div className="product-list">
+      <h3>Product List</h3>
+      <input
+        type=""
+        className="search-product-box"
+        placeholder="Search Product"
+        onChange={searchHandle}
+      />
+      <ul>
+        <li>Sr. No.</li>
+        <li>Name</li>
+        <li>Price</li>
+        <li>Category</li>
+        <li>Operation</li>
+      </ul>
+      {products.length > 0 ? (
+        products.map((item, index) => (
+          <ul key={item._id}>
+            <li>{index + 1}</li>
+            <li>{item.name}</li>
+            <li>{item.price}</li>
+            <li>{item.category}</li>
+            <li>
+              <button onClick={() => deleteProduct(item._id)}>Delete</button>
+              <button onClick={() => updateProduct(item._id)}>Update</button>
+            </li>
+          </ul>
+        ))
+      ) : (
+        <h1>No Result Found</h1>
+      )}
+    </div>
+  );
+};
 
 export default ProductList;
